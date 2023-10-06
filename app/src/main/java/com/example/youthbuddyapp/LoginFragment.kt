657173d8f436583cfbcf14ce1cafdd3bc8faadf8
cristianb18 <butnariucristian18@stud.ase.ro>
2023-10-07 02:30:41 +0300

    started firebase implementation

--- app/src/main/java/com/example/youthbuddyapp/LoginFragment.kt
+++ app/src/main/java/com/example/youthbuddyapp/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.youthbuddyapp
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -18,14 +19,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 class LoginFragment : Fragment() {
     private lateinit var binding: LoginFragmentBinding
-    private lateinit var auth : FirebaseAuth
-    private lateinit var googleSignInClient : GoogleSignInClient
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var database: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,12 +35,13 @@ class LoginFragment : Fragment() {
         binding = LoginFragmentBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         auth = FirebaseAuth.getInstance()
+        database = FirebaseFirestore.getInstance()
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
-        googleSignInClient = GoogleSignIn.getClient(requireContext() , gso)
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
 
         binding.signUpButton.setOnClickListener {
             signInGoogle()
@@ -48,38 +50,43 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
-    private fun signInGoogle(){
+    private fun signInGoogle() {
         val signInIntent = googleSignInClient.signInIntent
         launcher.launch(signInIntent)
     }
 
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-        if (result.resultCode == Activity.RESULT_OK){
-            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            handleResults(task)
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
         }
-    }
 
     private fun handleResults(task: Task<GoogleSignInAccount>) {
-        if (task.isSuccessful){
-            val account : GoogleSignInAccount? = task.result
-            if (account != null){
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
                 updateUI(account)
             }
-        }else{
-//            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun updateUI(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
         auth.signInWithCredential(credential).addOnCompleteListener {
-            Log.d("testt","success")
-            if (it.isSuccessful){
-                findNavController().navigate(R.id.from_login_to_info_1)
-            }else{
-//                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+            if (it.isSuccessful) {
+                Log.d("loginCheck", auth.currentUser?.uid.toString())
+                database.collection("users").document(auth.currentUser?.uid.toString()).get()
+                    .addOnSuccessListener { document ->
+                        if (!document.exists()) {
+                            database.collection("users").document(auth.currentUser?.uid.toString())
+                                .set(mapOf<String, Any>())
+                        }
+                    }
+                val intent = Intent(activity, PersonalDetailsActivity::class.java)
+                activity?.startActivity(intent)
+                //findNavController().navigate(R.id.from_login_to_info_1)
             }
         }
     }
--- app/src/main/java/com/example/youthbuddyapp/PersonalDetailsActivity.kt
+++ app/src/main/java/com/example/youthbuddyapp/PersonalDetailsActivity.kt
@@ -10,6 +10,12 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.youthbuddyapp.databinding.ActivityPersonalDetailsBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.File
+import java.io.FileInputStream
 
 
 class PersonalDetailsActivity : AppCompatActivity() {
@@ -19,11 +25,24 @@ class PersonalDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPersonalDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val db = Firebase.firestore
+        val auth = FirebaseAuth.getInstance()
+        val storage = Firebase.storage
+
 
         binding.editTextBox.setOnClickListener {
             val fileExplorerIntent = Intent(Intent.ACTION_GET_CONTENT)
             fileExplorerIntent.type = "application/*"
             filePickerActivityResult.launch(fileExplorerIntent)
+        }
+        binding.saveButton.setOnClickListener {
+            val user = hashMapOf(
+                "name" to binding.fullNameTextInputLayout.editText?.text.toString(),
+                "email" to binding.emailTextInputLayout.editText?.text.toString(),
+                "phone" to binding.phoneNumberTextInputLayout.editText?.text.toString()
+            )
+            db.collection("users").document(auth.currentUser?.uid.toString()).set(user).addOnSuccessListener {
+            }.addOnFailureListener { e-> Log.d("uploadCheck", e.toString()) }
         }
     }
 
@@ -31,6 +50,7 @@ class PersonalDetailsActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
                 binding.editTextBox.hint = getFileName(result.data?.data!!)
+                //val stream = FileInputStream(File((result.data?.data!!)).toString())
             }
         }
 
