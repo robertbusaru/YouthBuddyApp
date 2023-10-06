5320036f1c662350c902ba3b38a5a4a8af9ad9a3
robertbusaru <rbusaru@gmail.com>
2023-10-07 00:52:48 +0300

    Added: - bottom navigation bar - main menu activity - profile / jobs / saved / people

--- app/src/main/java/com/example/youthbuddyapp/MainActivity.kt
+++ app/src/main/java/com/example/youthbuddyapp/MainActivity.kt
@@ -15,17 +15,19 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.youthbuddyapp.databinding.ActivityMainBinding
 import com.example.youthbuddyapp.ui.theme.YouthBuddyAppTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        navController = navHostFragment.navController
+        navController.navigate(R.id.profileFragment)
     }
 }
