ef322a52f4285b81b42e0adf56758975faa79e19
robertbusaru <rbusaru@gmail.com>
2023-10-08 04:01:05 +0300

    edited login page

--- app/src/main/java/com/example/youthbuddyapp/ProfileFragment.kt
+++ app/src/main/java/com/example/youthbuddyapp/ProfileFragment.kt
@@ -1,13 +1,15 @@
 package com.example.youthbuddyapp
 
+import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.youthbuddyapp.databinding.ProfileFragmentBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -44,6 +46,12 @@ class ProfileFragment : Fragment() {
                 }
             }
 
+        binding.logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
 
         return binding.root
     }
